6d3fcd841592eb9715f9fb5a4d74cedcbb4c902a
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-04-29 22:48:27 -0300

    Teste 2 - Implementação

--- romanos.cpp
+++ romanos.cpp
@@ -3,11 +3,31 @@
 int romanos_para_decimal(char const * num_romano) {
 
     int resultado = 0;
+        // char* simbolos = "IVXLCDM";
 
     for (int i_char = 0; i_char < sizeof(num_romano); i_char++) {
         if (char(num_romano[i_char]) == int(*"I")) {
             resultado++;
         }
+        else if (char(num_romano[i_char]) == int(*"V")) {
+            resultado += 5;
+        }
+        else if (char(num_romano[i_char]) == int(*"X")) {
+            resultado += 10;
+        }
+        else if (char(num_romano[i_char]) == int(*"L")) {
+            resultado += 50;
+        }
+        else if (char(num_romano[i_char]) == int(*"C")) {
+            resultado += 100;
+        }
+        else if (char(num_romano[i_char]) == int(*"D")) {
+            resultado += 500;
+        }
+        else if (char(num_romano[i_char]) == int(*"M")) {
+            resultado += 1000;
+        }
+
     }
     return resultado;
 }
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -3,7 +3,13 @@
 #include "romanos.hpp"
 
 TEST_CASE("Números romanos - algarismos únicos", "[romanos]") {
-    REQUIRE(romanos_para_decimal("I") == 1);
+   REQUIRE(romanos_para_decimal("I") == 1);
+   REQUIRE(romanos_para_decimal("V") == 5);
+   REQUIRE(romanos_para_decimal("X") == 10);
+   REQUIRE(romanos_para_decimal("L") == 50);
+   REQUIRE(romanos_para_decimal("C") == 100);
+   REQUIRE(romanos_para_decimal("D") == 500);
+   REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
 /*
