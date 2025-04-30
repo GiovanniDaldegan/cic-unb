cbb25a54f51f5a57f82016d22e376e3ac6e65785
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-04-29 23:19:21 -0300

    Teste 4 - Implementação

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "romanos.hpp"
 
-TEST_CASE("Números romanos - algarismos únicos", "[require]") {
+TEST_CASE("Numeros romanos - algarismos unicos", "[require]") {
    REQUIRE(romanos_para_decimal("I") == 1);
    REQUIRE(romanos_para_decimal("V") == 5);
    REQUIRE(romanos_para_decimal("X") == 10);
@@ -12,7 +12,7 @@ TEST_CASE("Números romanos - algarismos únicos", "[require]") {
    REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
-TEST_CASE("Números romanos - números com mais de um algarismo", "[require]") {
+TEST_CASE("Numeros romanos - numeros com mais de um algarismo", "[require]") {
 	REQUIRE(romanos_para_decimal("II") == 2);
 	REQUIRE(romanos_para_decimal("VII") == 7);
 	REQUIRE(romanos_para_decimal("XVI") == 16);
@@ -21,11 +21,11 @@ TEST_CASE("Números romanos - números com mais de um algarismo", "[require]") {
 	REQUIRE(romanos_para_decimal("MDCLXVI") == 1666);
 }
 
-/*
+
 TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("G") == -1 );
-    
-    REQUIRE( romanos_para_decimal("i") == -1 );
-    
+    REQUIRE(romanos_para_decimal("G") == -1);
+    REQUIRE(romanos_para_decimal("i") == -1);
+    REQUIRE(romanos_para_decimal("#") == -1);
+	REQUIRE(romanos_para_decimal("Í") == -1);
 }
- */
+
