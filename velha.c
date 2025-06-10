f161567673a9935fccb55d2c601bde59c0c8cbef
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-10 20:27:14 -0300

    Teste 7 - sucesso

--- velha.c
+++ velha.c
@@ -8,6 +8,7 @@ int verifica_velha(int jogo[3][3])
   {
     for (int j = 0; j < 3; j++)
     {
+      // números de jogador inválidos
       if (jogo[i][j] < 0 || jogo[i][j] > 2)
         return -2;
 
@@ -18,8 +19,35 @@ int verifica_velha(int jogo[3][3])
     }
   }
 
+  // número inválido de jogadas
   if (jogadas2 > jogadas1 || jogadas1 > jogadas2 + 1)
     return -2;
+
+
+  int cont_lin = 0;
+  int num_anterior_lin = 0;
+
+  for (int i = 0; i < 3; i++)
+  {
+    cont_lin = 0; num_anterior_lin = 0;
+
+    for (int j = 0; j < 3; j++)
+    {
+      if (jogo[i][j] == num_anterior_lin)
+      {
+        cont_lin++;
+      }
+      else
+      {
+        cont_lin = 0;
+        num_anterior_lin = jogo[i][j];
+      }
+
+      if (cont_lin == 2 && num_anterior_lin != 0)
+        return num_anterior_lin;
+    }
+  }
+
 
   return -1;
 }
