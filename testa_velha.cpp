ae32486e11f92dc6403880cbd3e98232258f1de8
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-10 20:16:05 -0300

    Teste 7 - implementação

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -7,9 +7,7 @@
 #include "catch.hpp"
 #include "velha.h"
 
-int jogo_vazio[3][3] = {{0, 0, 0},
-                        {0, 0, 0},
-                        {0, 0, 0}};
+int jogo_vazio[3][3] = {{0}};
 
 void inicia_jogo(int jogo[3][3])
 {
@@ -137,5 +135,69 @@ TEST_CASE("Número de jogador inválido")
   REQUIRE(verifica_velha(jogo) == -2);
 }
 
+/** TESTE 7
+ */
+TEST_CASE("Jogador 1 vence na linha")
+{
+  int jogo[3][3];
 
+  inicia_jogo(jogo);
+  insere_movimento(jogo, 0, 0, 1); // 1 1 1
+  insere_movimento(jogo, 0, 1, 1); // 2 2 0
+  insere_movimento(jogo, 0, 2, 1); // 0 0 0
+
+  insere_movimento(jogo, 1, 0, 2);
+  insere_movimento(jogo, 1, 1, 2);
+  REQUIRE(verifica_velha(jogo) == 1);
+
+  
+  inicia_jogo(jogo);
+  insere_movimento(jogo, 2, 0, 1); // 0 0 2
+  insere_movimento(jogo, 2, 1, 1); // 0 2 0
+  insere_movimento(jogo, 2, 2, 1); // 1 1 1
+
+  insere_movimento(jogo, 0, 2, 2);
+  insere_movimento(jogo, 1, 1, 2);
+  REQUIRE(verifica_velha(jogo) == 1);
+}
+
+/*
+TEST_CASE("Jogador vence na coluna")
+{
+  inicia_jogo(jogo);
+  insere_movimento(jogo, 1, 1, 1); // 2 1 0
+  insere_movimento(jogo, 0, 1, 1); // 0 1 0
+  insere_movimento(jogo, 2, 1, 1); // 0 1 2
+
+  insere_movimento(jogo, 0, 0, 2);
+  insere_movimento(jogo, 2, 2, 2);
+  REQUIRE(verifica_velha(jogo) == 1);
+
+
+  inicia_jogo(jogo);
+  insere_movimento(jogo, 0, 0, 1); // 1 0 2
+  insere_movimento(jogo, 1, 1, 1); // 0 1 0
+  insere_movimento(jogo, 2, 2, 1); // 2 0 1
+
+  insere_movimento(jogo, 2, 0, 2);
+  insere_movimento(jogo, 0, 2, 2);
+  REQUIRE(verifica_velha(jogo) == 1);
+
+  
+  inicia_jogo(jogo);
+  insere_movimento(jogo, 0, 0, 1); // 1 1 1
+  insere_movimento(jogo, 0, 1, 1); // 1 2 2
+  insere_movimento(jogo, 0, 2, 1); // 2 1 2
+  insere_movimento(jogo, 1, 0, 1); 
+  insere_movimento(jogo, 2, 1, 1); 
+
+  insere_movimento(jogo, 1, 1, 2); 
+  insere_movimento(jogo, 1, 2, 2); 
+  insere_movimento(jogo, 2, 0, 2);
+  insere_movimento(jogo, 2, 2, 2);
+  REQUIRE(verifica_velha(jogo) == 1);
+}
+*/
+
+// TODO: teste x - os dois jogadores vencem
 // TODO: melhorar os títulos e adicionar descrições aos testes
