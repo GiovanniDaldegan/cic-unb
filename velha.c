258466c4ed239eebfe3695aefb563cef26ca01f5
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-10 16:42:35 -0300

    Teste 4 - sucesso

--- velha.c
+++ velha.c
@@ -2,14 +2,23 @@
 
 int verifica_velha(int jogo[3][3])
 {
+  int jogadas1 = 0, jogadas2 = 0;
+
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      if (jogo[i][j] == 2)
-        return -2;
+      if (jogo[i][j] == 1)
+        jogadas1++;
+      else if (jogo[i][j] == 2)
+        jogadas2++;
     }
   }
+
+  if (jogadas1 >= jogadas2)
+    return -1;
+  else
+    return -2;
   
   return -1;
 }
