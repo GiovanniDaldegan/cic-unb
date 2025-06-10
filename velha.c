fdde58bf4a530db10c654f91ad14f7ed2174a986
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-10 20:48:02 -0300

    Teste 8 - sucesso

--- velha.c
+++ velha.c
@@ -26,6 +26,7 @@ int verifica_velha(int jogo[3][3])
 
   int cont_lin = 0;
   int num_anterior_lin = 0;
+  int cont_col[2][3] = {{0}};
 
   for (int i = 0; i < 3; i++)
   {
@@ -33,6 +34,7 @@ int verifica_velha(int jogo[3][3])
 
     for (int j = 0; j < 3; j++)
     {
+      // checa vitória na linha
       if (jogo[i][j] == num_anterior_lin)
       {
         cont_lin++;
@@ -45,6 +47,20 @@ int verifica_velha(int jogo[3][3])
 
       if (cont_lin == 2 && num_anterior_lin != 0)
         return num_anterior_lin;
+
+
+      // checa vitória na coluna
+      if (jogo[i][j] == cont_col[0][j])
+      {
+        cont_col[1][j]++;
+      }
+      else {
+        cont_col[1][j] = 0;
+        cont_col[0][j] = jogo[i][j];
+      }
+
+      if (cont_col[1][j] == 2 && cont_col[0][j])
+        return cont_col[0][j];
     }
   }
 
